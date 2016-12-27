3040e0766726a37641be43e65c3c47d36e566641
John James Jacoby <johnjamesjacoby@me.com>
2016-12-26 21:31:09 -0600

    Numbers & classes.

--- wp-relationships/includes/metaboxes/relationship.php
+++ wp-relationships/includes/metaboxes/relationship.php
@@ -22,11 +22,11 @@ function wp_relationships_relationship_metabox( $relationship = null ) {
 	<table class="form-table">
 		<tr class="relationship-from-wrap">
 			<th><label for="relationship_from"><?php esc_html_e( 'From', 'wp-relationships' ); ?></label></th>
-			<td><input type="text" name="relationship_from" id="relationship_from" value="<?php echo esc_attr( $relationship->relationship_from ); ?>" class="regular-text" /></td>
+			<td><input type="number" name="relationship_from" id="relationship_from" value="<?php echo esc_attr( $relationship->relationship_from ); ?>" class="code" /></td>
 		</tr>
 		<tr class="relationship-top-wrap">
 			<th><label for="relationship_top"><?php esc_html_e( 'To', 'wp-relationships' ); ?></label></th>
-			<td><input type="text" name="relationship_top" id="relationship_to" value="<?php echo esc_attr( $relationship->relationship_to ); ?>" class="regular-text" /></td>
+			<td><input type="number" name="relationship_top" id="relationship_to" value="<?php echo esc_attr( $relationship->relationship_to ); ?>" class="code" /></td>
 		</tr>
 	</table>
 
