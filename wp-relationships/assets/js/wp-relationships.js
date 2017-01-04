bc855f63ce021da415d943d55c59df349e58f4d1
John James Jacoby <johnjamesjacoby@me.com>
2017-01-04 11:32:03 -0600

    First pass generic multi-row dynamic relationship connection metabox.

---
+++ wp-relationships/assets/js/wp-relationships.js
@@ -0,0 +1,50 @@
+jQuery( document ).ready( function( $ ) {
+    'use strict';
+
+	// UL rows are sortable
+	$( 'ul.new-relationship' ).sortable( {
+		items:     'li.add-new-relationship',
+		cursor:    'move',
+		axis:      'y',
+		cancel:    'a, input',
+		distance:  2,
+		opacity:   0.9,
+		tolerance: 'pointer',
+		scroll:    true
+	} );
+
+	// Delegate add/remove clicks
+	$( 'ul.new-relationship' ).on( 'click', 'a', function( e ) {
+		var row  = $( this ).parents( 'li' ),
+			rows = $( this ).parents( 'ul.new-relationship' ).children().length - 1;
+
+		// Add
+		if ( $( this ).hasClass( 'add-relationship' ) ) {
+
+			// Clone first row
+			var new_row = row.clone();
+
+			// Remove ID so datepicker can correctly target this new element
+			new_row
+				.hide()
+				.find( '.object-id' )
+					.val( '' )
+				.end()
+				.find( 'input:text' )
+					.val( '' )
+				.end()
+				.insertAfter( row )
+				.fadeIn( 400 );
+
+		// Remove (if not last row)
+		} else if ( $( this ).hasClass( 'remove-relationship' ) ) {
+			if ( rows > 1 ) {
+				row.slideUp( 200, function() {
+					row.remove();
+				} );
+			}
+		}
+
+		e.preventDefault();
+	} );
+} );
