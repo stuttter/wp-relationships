1188b971410ea4509917ab6a67771ad509473510
John James Jacoby <johnjamesjacoby@me.com>
2017-01-04 13:42:44 -0600

    Update connection metabox to be mostly error free.

--- wp-relationships/includes/metaboxes/connection.php
+++ wp-relationships/includes/metaboxes/connection.php
@@ -21,25 +21,23 @@ function wp_relationships_connection_metabox( $object = false ) {
 		return;
 	}
 
-	// Get child relationships
-	$default = array( (object) array( 'relationship_to_id' => 0, 'name' => '' ) );
-
 	// Relationship query
 	$connections = new WP_Relationship_Query( array(
-		'relationship_type' => 'post_post',
-		'from'                => $object->id
+		'type'    => 'post_post',
+		'from_id' => $object->ID
 	) );
 
-	// Setup relationships
-	$objects = ! empty( $connections )
-		? $connections
-		: $default; ?>
+	// Relationship types
+	$types = wp_get_relationships_of_type( 'post' );
 
-	<p class="description"><?php esc_html_e( 'Obligations can have multiple due-dates, each with a different amount. If this is a single relationship, only enter one due-date.', 'wp-relationships' ); ?></p>
+	// Setup relationships
+	$objects = ! empty( $connections->relationships )
+		? $connections->relationships
+		: array( WP_Relationship::get_instance() ); ?>
+
 	<ul class="new-relationship">
-		
-		<?php foreach ( $objects as $_object ) :
-			$_object = get_post( $_object->relationship_to_id ); ?>
+
+		<?php foreach ( $objects as $_object ) : ?>
 
 			<li class="add-new-relationship">
 				<div class="actions">
@@ -53,11 +51,24 @@ function wp_relationships_connection_metabox( $object = false ) {
 						<span class="screen-reader-text">
 							<?php esc_html_e( 'Remove Relationship', 'wp-relationships' ); ?>
 						</span>
-					</a>				
+					</a>
 				</div>
 
-				<input type="text"   name="relationship-to-id[]" class="relationship-to-id" value="<?php echo esc_attr( $_object->id ); ?>" />
-				<input type="hidden" name="relationship-id[]" class="relationship-id" value="<?php echo esc_attr( $_object->id ); ?>" />
+				<select name="relationship_type" id="type"><?php
+
+					// Loop through types
+					foreach ( $types as $type ) :
+
+						// Output type
+						?><option value="<?php echo esc_attr( $type->id ); ?>" <?php selected( $type->id, $_object->relationship_type, true ); ?>><?php echo esc_html( $type->name ); ?></option><?php
+
+					endforeach;
+
+				?></select>
+
+				<input type="hidden" name="relationship_from_id[]" class="relationship-from-id" value="<?php echo esc_attr( $object->id ); ?>" />
+				<input type="text"   name="relationship_to_id[]" class="relationship-to-id" value="<?php echo esc_attr( $_object->relationship_to_id ); ?>" />
+				<input type="hidden" name="relationship_id[]" class="relationship-id" value="<?php echo esc_attr( $_object->relationship_id ); ?>" />
 			</li>
 
 		<?php endforeach; ?>
